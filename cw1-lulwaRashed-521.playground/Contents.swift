866e0d0f60cec1f5a0c340149ec08459fcf3c629
lulwaRashed <looloo--2004@hotmail.com>
2020-06-13 19:13:10 +0300

    done

---
+++ cw1-lulwaRashed-521.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+let myName =  "LulwaALrashed"
+
+var age = 16
+var hight = 158
+var hobby1 = "swimming"
+var hobby2 = "cooking"
+
+print("my name is \(myName) my agee is \(age)  and my hight is \(hight)  my first hobby is \(hobby1) and my second hobby is \(hobby2)" )
+
+
+
