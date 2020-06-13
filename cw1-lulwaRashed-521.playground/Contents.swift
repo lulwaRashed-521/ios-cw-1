327fbc2e62e9dd20764a1f97a5ac47bd61e9bccc
lulwaRashed <looloo--2004@hotmail.com>
2020-06-13 20:08:53 +0300

    done

--- cw1-lulwaRashed-521.playground/Contents.swift
+++ cw1-lulwaRashed-521.playground/Contents.swift
@@ -12,5 +12,14 @@ var hobby2 = "cooking"
 
 print("my name is \(myName) my agee is \(age)  and my hight is \(hight)  my first hobby is \(hobby1) and my second hobby is \(hobby2)" )
 
+let friendName = "sara"
+var friendHight = 156
+var friendAge = 15
+var friendHobby1 = "paintig"
+var friendHobby2 = "cooking"
+var age_difference = age - friendAge
+
+age > friendAge
+hight == friendHight
 
 
